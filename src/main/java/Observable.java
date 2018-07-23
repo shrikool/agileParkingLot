b27141c9915293aced0458fec6bf43b29b093d4e
Shrikant Pandit <shrikool@gmail.com>
2018-07-23 13:20:17 +0530

    Removing old files

--- src/main/java/Observable.java
+++
@@ -1,6 +0,0 @@
-public interface Observable {
-    public final String FBIEvent = "FBI_EVENT";
-    public final String OwnerEvent = "OWNER_EVENT";
-    public void registerObserver(Object personToNotify);
-    public void notifyObservers(String eventType,Object result);
-}
--- src/main/java/Observer.java
+++
@@ -1,8 +0,0 @@
-
-/**
- * Created by shrikant on 4/23/2015.
- */
-public interface Observer {
-
-    public void update(Observable parkingLot, Object parkingLotStatus);
-}
